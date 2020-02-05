fdbb87b66add2e85d0ecbd7d8a16daf96317bdb1
adwaya <adwaya@live.in>
2020-02-05 22:41:29 +0000

    first api project
    json

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+info = [
+    {
+        'creator_name': 'adwaya',
+        'version': '2.0',
+        'species': 'humanoid',
+        'entertainment': {
+            'games': 'PUBG',
+            'to-do': 'learning to think like a coder'
+        }
+    }
+]
+
+#GET /info
+@app.route('/info')
+def get_info():
+    return jsonify({'info': info})
+
+app.run(host="0.0.0.0",port=5000,debug=True,use_reloader=True)
